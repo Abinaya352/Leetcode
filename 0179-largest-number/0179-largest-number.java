ccff2a1a91a9833aff8ff75ce1891a79a6e6b08c
Abinaya Prakash <161126164+Abinaya352@users.noreply.github.com>
2024-09-18 08:38:02 +0530

    Time: 5 ms (98.24%), Space: 42.9 MB (76.31%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -3,23 +3,17 @@ class Solution {
         int i,j,n=nums.length;
         String[] arr=new String[n];
         for(i=0;i<n;i++){
-            arr[i]=nums[i]+"";
+            arr[i]=String.valueOf(nums[i]);
         }
-        String ans="";
-        for(i=0;i<n-1;i++){
-            for(j=0;j<n-i-1;j++){
-                if((arr[j+1]+arr[j]).compareTo(arr[j]+arr[j])>0){
-                    String t=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=t;
-                }
-            }
-        }
+        Arrays.sort(arr,(a,b)->(b+a).compareTo(a+b));
         if(arr[0].equals("0")){
-            System.out.print("yes");
             return "0";
         }
-        return String.join("", arr);
+        StringBuilder sb=new StringBuilder();
+        for(String str:arr){
+            sb.append(str);
+        }
+        return sb.toString();
         
     }
 }
