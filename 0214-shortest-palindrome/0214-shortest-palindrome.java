579b36e3afc6265b1e1553925f72fc334da8772a
Abinaya Prakash <161126164+Abinaya352@users.noreply.github.com>
2024-09-20 22:44:35 +0530

    Time: 8 ms (63.77%), Space: 44.4 MB (86.64%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.java
+++ 0214-shortest-palindrome/0214-shortest-palindrome.java
@@ -1,28 +1,32 @@
 class Solution {
     public String shortestPalindrome(String s) {
-        int count=kmp(new StringBuilder(s).reverse().toString(),s);
+        int count = kmp(s);  // We pass only the original string
         return new StringBuilder(s.substring(count)).reverse().toString() + s;
     }
-    public int kmp(String reversedString,String original){
-        String modified=reversedString+"#"+original;
-        int len=modified.length();
-        int i=1,k=0;
-        int[] lps=new int[len];
-        while(i<len){
-            if(modified.charAt(i)==modified.charAt(k)){
-                k++;
-                lps[i]=k;
-                i++;
 
-            }else{
-                if(k>0){
-                    k=lps[k-1];
-                }else{
-                    lps[i]=0;
+    public int kmp(String original) {
+        String reversedString = new StringBuilder(original).reverse().toString();
+        String modified = original + "#" + reversedString;  // Modify the string
+
+        int len = modified.length();
+        int[] lps = new int[len];  // LPS array
+        int i = 1, k = 0;
+
+        while (i < len) {
+            if (modified.charAt(i) == modified.charAt(k)) {
+                k++;
+                lps[i] = k;
+                i++;
+            } else {
+                if (k > 0) {
+                    k = lps[k - 1];
+                } else {
+                    lps[i] = 0;
                     i++;
                 }
             }
         }
-        return lps[len-1];
+        
+        return lps[len - 1];  // This gives the longest palindromic prefix length
     }
 }
